a7052c817881b3f54ce2c3b44fd144b15bc4d6be
Romulo Rosa <romulothiago@furg.br>
2019-06-17 09:31:37 -0300

    HP-10 Add get order

--- hercules_framework/talkers/__init__.py
+++ hercules_framework/talkers/__init__.py
@@ -1,14 +1,19 @@
+from typing import Union
+
+from hercules_framework.models.base import BaseModel
 from hercules_framework.models.crud_response import (ClientModel, OrderModel,
                                                      SellerModel)
-from hercules_framework.settings import CRUD_CLIENT_HOST, CRUD_ORDER_HOST, CRUD_SELLER_HOST
-from .base import BaseTalker
+from hercules_framework.settings import (CRUD_CLIENT_HOST, CRUD_ORDER_HOST,
+                                         CRUD_SELLER_HOST)
+
+from .base import BaseTalker, session
 
 
 class SellerTalker(BaseTalker):
     base_url = 'hercules/crud-seller/v1/seller'
     model = SellerModel
 
-    def __init__(self, host: str=CRUD_SELLER_HOST):
+    def __init__(self, host: str = CRUD_SELLER_HOST):
         self.host = host
 
 
@@ -16,13 +21,20 @@ class OrderTalker(BaseTalker):
     base_url = 'hercules/crud-order/v1/order'
     model = OrderModel
 
-    def __init__(self, host: str=CRUD_ORDER_HOST):
+    def __init__(self, host: str = CRUD_ORDER_HOST):
         self.host = host
+
+    def get(self, id: str, header: dict = {}, first=True) -> Union[BaseModel, None]:
+        header.update(self._base_header)
+        url = '/'.join([self.host, self.base_url, '']) + f'?orderId={id}'
+        response: Response = session.get(
+            url=url, headers=header)
+        return self._treat_response(response.json(), first=first)
 
 
 class CLientTalker(BaseTalker):
     base_url = 'hercules/crud-client/v1/client'
     model = ClientModel
 
-    def __init__(self, host: str=CRUD_CLIENT_HOST):
+    def __init__(self, host: str = CRUD_CLIENT_HOST):
         self.host = host
--- hercules_framework/talkers/base.py
+++ hercules_framework/talkers/base.py
@@ -14,11 +14,7 @@ class BaseTalker:
     model: BaseModel = None
     _base_header = {'Content-Type': 'application/json'}
 
-    def get(self, id: str, header: dict = {}, first=True) -> Union[BaseModel, None]:
-        header.update(self._base_header)
-        url = '/'.join([self.host, self.base_url, str(id)])
-        response: Response = session.get(url=url + '?limit=1', headers=header)
-        json_response = response.json()
+    def _treat_response(self, json_response: dict, first):
         if json_response is not None:
             resp = CRUDResponse.from_dict(json_response, self.model).data
             if first:
@@ -26,9 +22,14 @@ class BaseTalker:
             return resp
         return None
 
+    def get(self, id: str, header: dict = {}, first=True) -> Union[BaseModel, None]:
+        header.update(self._base_header)
+        url = '/'.join([self.host, self.base_url, str(id)])
+        response: Response = session.get(url=url + '?limit=1', headers=header)
+        return self._treat_response(response.json(), first=first)
+
     def put(self, body: dict, header: dict = {}) -> int:
         header.update(self._base_header)
         url = '/'.join([self.host, self.base_url])
         response: Response = session.post(url=url, json=body, headers=header)
         return response.status_code
-
